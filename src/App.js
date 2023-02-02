857b9bb38b16db6d6a194aed9fc87f3fdbbb142d
Clevin <64676900+clevinwilson@users.noreply.github.com>
2023-02-02 16:27:17 +0530

    Index page message updated

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Hi there! how are you?
+        Docker
         </p>
         <a
           className="App-link"
