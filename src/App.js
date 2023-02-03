ddf0b03d9d522cfd17538dc6c070b6a9a181d67f
mohdjishin <whoistcp@protonmail.com>
2023-02-03 21:09:57 +0530

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        git action check this
+        git action check this out
         </p>
         <a
           className="App-link"
